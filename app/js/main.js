09cb603a964ca6a157234ded196df8e618e32092
Martin <martinstudyweb@gmail.com>
2021-10-25 19:26:19 +0300

    add ttf2woff, finl-include, functin for fonts and fontStyles

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
 $(function(){
   $('body').css('background-color', 'grey');
+  $('h1').css('color', 'blue');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
 // get variables
-const scss       = require('gulp-sass')(require('sass'));
 //** or for scss we can use other method
 /*  import dartSass from 'sass'
     import gulpSass from 'gulp-sass'
     const sass = gulpSass(dartSass)
 */
+const scss       = require('gulp-sass')(require('sass'));
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
 const uglify       = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const imagemin     = require('gulp-imagemin');
 const del          = require('del');
+const fileinclude  = require('gulp-file-include'); 
+const ttf2woff     = require('gulp-ttf2woff');
+const ttf2woff2    = require('gulp-ttf2woff2');
+const imagemin     = require('gulp-imagemin');
+
+let fs = require('fs');
 
 // for reload browser when did some change
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: 'dist/'
     }
   });
 }
@@ -31,20 +36,26 @@ function cleanDist() {
 // for min images and sage good quality
 function images() {
   return src('app/images/**/*')
-    .pipe(imagemin(
-      [
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.mozjpeg({quality: 75, progressive: true}),
-        imagemin.optipng({optimizationLevel: 5}),
-        imagemin.svgo({
-          plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-          ]
-        })
-      ]
-    ))
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
     .pipe(dest('dist/images'))
+}
+
+// for get html files and concat it
+function html() {
+  return src(['app/*.html', '!app/_*.html'])
+    .pipe(fileinclude())
+    .pipe(dest('dist/'))
+    .pipe(browserSync.stream())
 }
 
 // forking with scripts and min their
@@ -53,12 +64,13 @@ function scripts() {
     'node_modules/jquery/dist/jquery.min.js',
     'app/js/main.js'
   ])
+  .pipe(fileinclude())
   // before we min js filse we should contac all of them
   .pipe(concat('main.min.js'))
   // after min them
   .pipe(uglify())
   // after send all of them in app/js folder
-  .pipe(dest('app/js'))
+  .pipe(dest('dist/js'))
   // after reload browser
   .pipe(browserSync.stream())
 }
@@ -77,42 +89,85 @@ function styles() {
       grid: true
     }))
     // send the finish css file to the src
-    .pipe(dest('app/css'))
+    .pipe(dest('dist/css'))
     // now tracking these file by using browserSync
     .pipe(browserSync.stream())
 }
 
-// for build files in dist folder production
-function build() {
-  return src([
-    'app/css/style.min.css',
-    'app/fonts/**/*',
-    'app/js/main.min.js',
-    'app/*.html'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+// convert .ttf2woff to woff2
+function fonts() {
+  src("app/fonts/*.ttf")
+    .pipe(ttf2woff())
+    .pipe(dest('dist/fonts/'));
+  return src("app/fonts/*.ttf")
+    .pipe(ttf2woff2())
+    .pipe(dest('dist/fonts/'));
 }
+
+// get all fonts and add path for them
+function fontsStyle() {
+  let fileContent = fs.readFileSync('app/scss/fonts.scss');
+  console.log(fileContent)
+  if (fileContent == '') {
+    fs.writeFile('app/scss/fonts.scss', '', cb);
+    return fs.readdir('dist/fonts/', function (err, items) {
+      if (items) {
+        let cFontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (cFontname != fontname) {
+            fs.appendFile('app/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+          }
+          cFontname = fontname;
+        }
+      }
+    })
+  }
+}
+
+function cb() {}
+
+// for build files in dist folder production
+// function build() {
+//   return src([
+//     'app/css/style.min.css',
+//     'app/fonts/**/*',
+//     'app/js/main.min.js',
+//     '!app/_*.html'
+//   ], {base: 'app'})
+//   .pipe(dest('dist/'))
+// }
 
 // function for tracking changes
 function watching() {
   // gulp.watch starting to track the files and 
+  watch(['app/*.html'], html);
   // start using styles if it need
   watch(['app/scss/**/*.scss'], styles);
   // tracking all files js exept !main.min.js
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  // tracking after all images
+  watch('app/images/**/*.{jpg,png,svg,gif,ico,webp}', images);
+  // reload browser when has some changing in html file
   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
+let build   = series(cleanDist, parallel(scripts, styles, html, images, fonts), fontsStyle); 
+// when we starting gulp by write gulp the both function will be strated
+exports.default = parallel(build, watching, browsersync);
+
+exports.html        = html;
+exports.fonts       = fonts;
+exports.fontsStyle  = fontsStyle;
 exports.styles      = styles;
 exports.watching    = watching;
 exports.browsersync = browsersync;
 exports.scripts     = scripts;
 exports.imagemin    = imagemin;
 exports.cleanDist   = cleanDist;
+exports.build       = build;
 
-exports.build   = series(cleanDist, images, build); 
-// when we starting gulp by write gulp the both function will be strated
-exports.default = parallel(styles, scripts, browsersync, watching);
 
 // async function builds() { scss() };
 // export { builds };
